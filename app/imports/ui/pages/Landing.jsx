416265aa25c36ecca9b646a95c9ff48825eb39c2
JustKCorp <45214579+JustKCorp@users.noreply.github.com>
2024-04-16 11:43:58 -1000

    Changed color scheme and splash page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,53 +7,13 @@ const Landing = () => (
   <div id={PageIDs.landingPage}>
     <div className="landing-green-background">
       <Container className="text-center">
-        <h1 style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}>
+        <h1 style={{ paddingTop: '20px', color: 'black', fontSize: '36pt' }}>
           Explore
         </h1>
-        <h3 style={{ paddingBottom: '20px', color: 'white' }}>
+        <h3 style={{ paddingBottom: '20px', color: 'black' }}>
           Clubs at U.H Manoa that are eager for you to join
+          <Image src="/images/profiles-page.png" width={500} />
         </h3>
-      </Container>
-    </div>
-    <div className="landing-white-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Sign in with your UH account</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/home-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/profiles-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-green-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...add your interests</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/add-project-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/projects-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-white-background text-center">
-      <h2 style={{ color: '#376551' }}>
-        Browse around and find the right club for you!
-      </h2>
-      <Container>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/interests-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/filter-page.png" width={500} />
-          </Col>
-        </Row>
       </Container>
     </div>
   </div>
