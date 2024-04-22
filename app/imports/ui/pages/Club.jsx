94b41088845b0492dfc4937c48209d79d620a63b
Kendrick Gonzales <kg33@hawaii.edu>
2024-04-21 23:40:00 -1000

    trying to add clublist onto webpage
    ok

--- app/imports/ui/pages/Club.jsx
+++ app/imports/ui/pages/Club.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
-import { ExternalLinkIcon } from 'react-bootstrap-icons'; // Import the icon for external links
+import { Container } from 'react-bootstrap';
 import { PageIDs } from '../utilities/ids';
 
 const ClubPage = () => (
@@ -8,9 +7,7 @@ const ClubPage = () => (
     <h1>Welcome to our Club Page!</h1>
     <p>Explore our clubs and find what interests you.</p>
     {/* Button to link to the club list website */}
-    <Button variant="primary" href="https://clubmeetup.github.io/clublist/" target="_blank" rel="noopener noreferrer">
-      Explore Clubs <ExternalLinkIcon size={20} />
-    </Button>
+    <a style={{ color: 'white' }} href="https://clubmeetup.github.io/clublist/">Club Directory/</a>
   </Container>
 );
 
