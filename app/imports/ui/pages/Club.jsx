c1196d5fffd38622c657e5d404e911872c2d07a4
Kendrick Gonzales <kg33@hawaii.edu>
2024-04-21 23:01:50 -1000

    took out extra space

--- app/imports/ui/pages/Club.jsx
+++ app/imports/ui/pages/Club.jsx
@@ -3,7 +3,6 @@ import { Container, Button } from 'react-bootstrap';
 import { ExternalLinkIcon } from 'react-bootstrap-icons'; // Import the icon for external links
 import { PageIDs } from '../utilities/ids';
 
-
 const ClubPage = () => (
   <Container id={PageIDs.clubPage}>
     <h1>Welcome to our Club Page!</h1>
