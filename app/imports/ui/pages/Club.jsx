179f6da61b4baa1f8b231d76eaa9945dcf68472e
Kendrick Gonzales <kg33@hawaii.edu>
2024-05-07 01:05:33 -1000

    updated club.jsx

--- app/imports/ui/pages/Club.jsx
+++ app/imports/ui/pages/Club.jsx
@@ -6,8 +6,9 @@ const ClubPage = () => (
   <Container id={PageIDs.clubPage}>
     <h1>Welcome to our Club Page!</h1>
     <p>Explore our clubs and find what interests you.</p>
+
     {/* Button to link to the club list website */}
-    <a style={{ color: 'white' }} href="https://clubmeetup.github.io/clublist/">Club Directory/</a>
+    <a style={{ color: 'black' }} href="https://clubmeetup.github.io/clublist/">Club Directory/</a>
   </Container>
 );
 
